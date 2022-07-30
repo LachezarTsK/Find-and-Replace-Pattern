058f4cadb5af7edf4cb19bba9a178c10d959ec13
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-07-30 15:43:40 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    static final int ALPHABET_SIZE = 26;
+    static final char EMPTY = '\u0000';
+
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+
+        List<String> allWordsThatMatchPattern = new ArrayList<>();
+        for (String word : words) {
+            if (wordMatchesPattern(word, pattern)) {
+                allWordsThatMatchPattern.add(word);
+            }
+        }
+        return allWordsThatMatchPattern;
+    }
+
+    private boolean wordMatchesPattern(String word, String pattern) {
+        char[] patternToWord = new char[ALPHABET_SIZE];
+        char[] wordToPattern = new char[ALPHABET_SIZE];
+
+        for (int i = 0; i < word.length(); ++i) {
+            char p = pattern.charAt(i);
+            char w = word.charAt(i);
+            if ((patternToWord[p - 'a'] != EMPTY && patternToWord[p - 'a'] != w)
+                    || (wordToPattern[w - 'a'] != EMPTY && wordToPattern[w - 'a'] != p)) {
+                return false;
+            }
+            patternToWord[p - 'a'] = w;
+            wordToPattern[w - 'a'] = p;
+        }
+        return true;
+    }
+}
